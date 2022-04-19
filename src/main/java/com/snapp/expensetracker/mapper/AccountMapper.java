379501ab32f0f0bfba1ae0731f877b104c3070f2
ali <ali.esmaily65@gmail.com>
2022-04-19 23:49:23 +0430

    feat(Account):
    account business have been implemented.
    tests are not passed.
    
    Closes task-2

---
+++ src/main/java/com/snapp/expensetracker/mapper/AccountMapper.java
@@ -0,0 +1,23 @@
+package com.snapp.expensetracker.mapper;
+
+import com.snapp.expensetracker.entity.Account;
+import com.snapp.expensetracker.model.AccountDto;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class AccountMapper {
+
+    public static AccountDto toDto(Account account) {
+       return AccountDto.builder().accountType(account.getAccountType())
+               .stateType(account.getStateType()).balance(account.getBalance())
+               .creationDate(account.getCreationDate()).holder(account.getHolder()).build();
+    }
+
+    public static Account toAccount(AccountDto accountDto) {
+        return Account.builder().accountType(accountDto.getAccountType())
+                .balance(accountDto.getBalance())
+                .holder(accountDto.getHolder()).build();
+    }
+
+}
